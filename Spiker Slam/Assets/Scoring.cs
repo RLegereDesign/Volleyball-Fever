4aefb789c3eb3e3a896ece9e1d4a2b5f370ff3df
Riley L <rlegeredesign@gmail.com>
2018-03-24 00:02:04 -0400

    Added Score Counter and spawns
    Added score detection, counter, and ball spawn for player who last scored.

---
+++ Spiker Slam/Assets/Scoring.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Scoring : MonoBehaviour {
+
+	public Text scoreDisplay;
+	public GameObject Ball;
+	public float xAx;
+	public float yAx;
+
+	private int score = 0;
+	private bool ballCheck = true;
+
+	// Use this for initialization
+	void Start () {
+		scoreDisplay.text = "Score: 0";
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Ball")
+		{
+			score++;
+			scoreDisplay.text = ("Score: "+score);
+			StartCoroutine (SpawnServe ());
+			StopCoroutine (SpawnServe ());
+			Destroy (other.gameObject);
+			ballCheck = false;
+
+		}
+			
+	}
+
+
+	IEnumerator SpawnServe ()
+	{
+		yield return new WaitForSeconds (2);
+
+		if (ballCheck == false) {
+			Vector3 Spawn = new Vector3 (xAx, yAx, 0.0f);
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate (Ball, Spawn, spawnRotation);
+			ballCheck = true;
+		}
+}
+}
---
+++ Spiker Slam/Assets/Serve.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Serve : MonoBehaviour {
+
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		
+		rb = GetComponent<Rigidbody2D> ();
+		rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") {
+			rb.constraints = RigidbodyConstraints2D.None;
+		}
+		
+	}
+}
